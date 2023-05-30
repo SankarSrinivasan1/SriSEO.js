3c54f9035e2940f70b012c2e9d2cac15f67e06b4
Sankar Srinivasan <petra.srini@gmail.com>
2023-05-30 09:09:25 +0530

    Update SriSEO.js

--- SriSEO.js
+++ SriSEO.js
@@ -1,279 +1,165 @@
-// SEOify.js
-
+// SriSEO.js
 // Namespace for the library
 
-var SEOify = {};
+var SriSEO = {};
 
 // Function to set meta tags
-
-SEOify.setMetaTags = function (metaData) {
-
+SriSEO.setMetaTags = function (metaData) {
   var head = document.head || document.getElementsByTagName('head')[0];
 
   // Remove existing meta tags
-
   var existingMetaTags = document.querySelectorAll('meta[name="description"], meta[name="keywords"]');
-
   existingMetaTags.forEach(function (tag) {
-
     head.removeChild(tag);
-
   });
 
   // Create new meta tags
-
   var descriptionTag = document.createElement('meta');
-
   descriptionTag.name = 'description';
-
   descriptionTag.content = metaData.description;
-
   head.appendChild(descriptionTag);
 
   var keywordsTag = document.createElement('meta');
-
   keywordsTag.name = 'keywords';
-
   keywordsTag.content = metaData.keywords.join(', ');
-
   head.appendChild(keywordsTag);
-
 };
 
 // Function to generate URL slug
-
-SEOify.generateSlug = function (url) {
-
+SriSEO.generateSlug = function (url) {
   return url.toLowerCase().trim().replace(/\s+/g, '-');
-
 };
 
 // Function to set canonical URL
-
-SEOify.setCanonicalURL = function (url) {
-
+SriSEO.setCanonicalURL = function (url) {
   var linkTag = document.createElement('link');
-
   linkTag.rel = 'canonical';
-
   linkTag.href = url;
-
   document.head.appendChild(linkTag);
-
 };
 
 // Function to add structured data markup
-
-SEOify.addStructuredData = function (data) {
-
+SriSEO.addStructuredData = function (data) {
   var scriptTag = document.createElement('script');
-
   scriptTag.type = 'application/ld+json';
-
   scriptTag.innerHTML = JSON.stringify(data);
-
   document.head.appendChild(scriptTag);
-
 };
 
 // Function to generate XML sitemap
-
-SEOify.generateXMLSitemap = function (urls) {
-
+SriSEO.generateXMLSitemap = function (urls) {
   var xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
-
   xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
-
-  
-
   urls.forEach(function (url) {
-
     xml += '  <url>\n';
-
     xml += '    <loc>' + url + '</loc>\n';
-
     xml += '  </url>\n';
-
   });
-
   xml += '</urlset>';
-
   return xml;
-
 };
 
 // Function to set Open Graph tags
-
-SEOify.setOpenGraphTags = function (data) {
-
+SriSEO.setOpenGraphTags = function (data) {
   var head = document.head || document.getElementsByTagName('head')[0];
 
   // Remove existing Open Graph tags
-
   var existingOGTags = document.querySelectorAll('meta[property^="og:"]');
-
   existingOGTags.forEach(function (tag) {
-
     head.removeChild(tag);
-
   });
 
   // Create new Open Graph tags
-
   for (var property in data) {
-
     var ogTag = document.createElement('meta');
-
     ogTag.setAttribute('property', 'og:' + property);
-
     ogTag.content = data[property];
-
     head.appendChild(ogTag);
-
   }
-
 };
 
 // Function to set Twitter Card tags
-
-SEOify.setTwitterCardTags = function (data) {
-
+SriSEO.setTwitterCardTags = function (data) {
   var head = document.head || document.getElementsByTagName('head')[0];
 
   // Remove existing Twitter Card tags
-
   var existingTwitterTags = document.querySelectorAll('meta[name^="twitter:"]');
-
   existingTwitterTags.forEach(function (tag) {
-
     head.removeChild(tag);
-
   });
 
   // Create new Twitter Card tags
-
   for (var property in data) {
-
     var twitterTag = document.createElement('meta');
-
     twitterTag.setAttribute('name', 'twitter:' + property);
-
     twitterTag.content = data[property];
-
     head.appendChild(twitterTag);
-
   }
-
 };
 
 // Function to perform SEO analysis and provide recommendations
-
-SEOify.analyzeAndRecommendations = function () {
-
+SriSEO.analyzeAndRecommendations = function () {
   // Perform SEO analysis on the webpage
-
   // ...
-
   // Generate recommendations based
-
-  
-
   var pageTitle = document.title;
-
   var metaDescription = document.querySelector('meta[name="description"]').getAttribute('content');
-
   var headingTags = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
-
   var imageTags = document.querySelectorAll('img');
 
   // Analyze the webpage elements and gather SEO data
-
   var analysisResults = {
-
     title: pageTitle,
-
     description: metaDescription,
-
     headingCount: headingTags.length,
-
     imageCount: imageTags.length,
-
     // ... Other SEO analysis data
-
   };
 
   // Generate recommendations based on the analysis
-
   var recommendations = [];
-
   if (pageTitle.length > 70) {
-
     recommendations.push("Shorten the page title to improve readability and search engine visibility.");
-
   }
 
   if (metaDescription.length > 160) {
-
     recommendations.push("Optimize the meta description to be within the recommended length of 160 characters.");
-
   }
 
   if (headingTags.length < 1) {
-
     recommendations.push("Include at least one heading tag (h1 - h6) on the webpage for better structure and SEO.");
-
   }
 
   if (imageTags.length < 5) {
-
     recommendations.push("Add more relevant images to enhance the visual appeal and engagement of the webpage.");
-
   }
 
   // Return the analysis results and recommendations
-
   return {
-
     analysis: analysisResults,
-
     recommendations: recommendations
-
   };
-
 };
 
 // Function to check compatibility and performance
-
-SEOify.checkCompatibilityAndPerformance = function () {
+SriSEO.checkCompatibilityAndPerformance = function () {
 
   // Check browser compatibility
-
   var isCompatible = true; // Perform browser compatibility checks
 
   // Check performance metrics
-
   var performanceScore = 90; // Calculate performance score based on performance metrics
 
   // Return the compatibility and performance results
-
   return {
-
     compatible: isCompatible,
-
     performance: performanceScore
-
   };
-
 };
 
 // Export the SEOify object for use in other scripts
-
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-
-  module.exports = SEOify;
-
+  module.exports = SriSEO;
 } else {
-
-  window.SEOify = SEOify;
-
+  window.SriSEO = SriSEO;
 }
